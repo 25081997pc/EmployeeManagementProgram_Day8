3fd582157eb2f328e891dff51c76d9545351ec9a
25081997pc <chavanpooja2508@gmail.com>
2022-07-18 13:50:50 +0530

    Welcome message

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeManagement_Day8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Simulate Employee Wage Program");
+        }
+    }
+   
+}
