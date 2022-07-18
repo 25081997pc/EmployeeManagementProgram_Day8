ed288f01ec8012c78a31a0d595cb2a01db885663
25081997pc <chavanpooja2508@gmail.com>
2022-07-18 14:58:30 +0530

    Calculating Employee Wage

--- EmpWage.cs
+++ EmpWage.cs
@@ -8,23 +8,34 @@ namespace EmployeeManagement_Day8
 {
     public class EmpWage
     {
-        //This Program is used to check whether employee is present or absent
+        //This Program is used to calculate employee wage
+        //Constant
+        public const int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR;
+        //Constructor
+        public EmpWage(int ERPH)
+        {
+            EMP_RATE_PER_HOUR = ERPH;
+        }
         public void EmpCheck()
         {
-            //Constant
-            int IS_FULL_TIME = 1;
+            //Variables
+            int empHrs = 0;
+            int empWage = 0;
 
             Random random = new Random();
             //Computation
             int empChk = random.Next(2);
             if (empChk == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee Wage is : " + empWage);
         }
     }
 }
