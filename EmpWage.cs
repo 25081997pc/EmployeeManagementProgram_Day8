7440117186cc94bf8e18f4a20215c9839acd5c11
25081997pc <chavanpooja2508@gmail.com>
2022-07-18 15:55:41 +0530

    Calculating EmpWage For Number of working days

--- EmpWage.cs
+++ EmpWage.cs
@@ -8,39 +8,46 @@ namespace EmployeeManagement_Day8
 {
     public class EmpWage
     {
-        //This Program is used to calculate employee wage
+        //This Program is used to calculate employee Dailywage and total EmpWage for num of working days
         //Constant
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
         int EMP_RATE_PER_HOUR;
+        int NUM_OF_WORKING_DAYS;
         //Constructor
-        public EmpWage(int ERPH)
+        public EmpWage(int ERPH, int NOWD)
         {
             EMP_RATE_PER_HOUR = ERPH;
+            NUM_OF_WORKING_DAYS = NOWD;
         }
         public void EmpCheck()
         {
             //Variables
             int empHrs = 0;
             int empWage = 0;
-
-            Random random = new Random();
-            //Computation
-            int empChk = random.Next(3);
-            switch (empChk)
+            int totalEmpWage = 0;
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
             {
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                //Computation
+                int empChk = random.Next(3);
+                switch (empChk)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                Console.WriteLine("Day# : " + day + " Emp Daily Wage : " + empWage);
+                totalEmpWage += empWage;
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Employee Wage is : " + empWage);
+            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
         }
     }
 }
