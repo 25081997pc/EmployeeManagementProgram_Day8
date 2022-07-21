e51e445f61f982c50e69e7746c56d7efa43ee644
25081997pc <chavanpooja2508@gmail.com>
2022-07-21 12:01:16 +0530

    Using Reference

--- Program.cs
+++ Program.cs
@@ -7,7 +7,6 @@
             Console.WriteLine("Welcome to Employee Management Program");
 
             IComputeEmpWage empWageBuilderObject = new EmpWageBuilderObject();
-
             empWageBuilderObject.addCompanyEmpWage("DMart", 20, 4, 20);
             empWageBuilderObject.addCompanyEmpWage("Reliance", 18, 2, 10);
             empWageBuilderObject.computeEmpWage();
