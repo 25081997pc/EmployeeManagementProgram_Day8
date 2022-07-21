c97c221da6dde648e48267fbaeb9efe496eddbdf
25081997pc <chavanpooja2508@gmail.com>
2022-07-21 13:32:43 +0530

    calling method getTotalWage with company

--- Program.cs
+++ Program.cs
@@ -6,10 +6,12 @@
         {
             Console.WriteLine("Welcome to Employee Management Program");
 
-            IComputeEmpWage empWageBuilderObject = new EmpWageBuilderObject();
+            EmpWageBuilderObject empWageBuilderObject = new EmpWageBuilderObject();
             empWageBuilderObject.addCompanyEmpWage("DMart", 20, 4, 20);
             empWageBuilderObject.addCompanyEmpWage("Reliance", 18, 2, 10);
             empWageBuilderObject.computeEmpWage();
+            Console.WriteLine("Total Wage for DMart company : " + empWageBuilderObject.getTotalWage("DMart"));
+            Console.WriteLine("Total Wage for Reliance company : " + empWageBuilderObject.getTotalWage("Reliance"));
         }
     }
 }
